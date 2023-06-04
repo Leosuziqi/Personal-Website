fc16b725720b46a59cd8f723103d54e2782ec541
Leosuziqi <50681212+Leosuziqi@users.noreply.github.com>
2023-06-04 15:48:14 -0700

    Update logodisplay.js

--- assets/js/logodisplay.js
+++ assets/js/logodisplay.js
@@ -2,12 +2,12 @@ const center = document.getElementById("center");
 center_width = center.offsetWidth;
 //console.log(center_width)
 center.style.marginLeft = Math.round(600-center_width/2) + "px";
-center.style.marginTop = 400 + "px";
+center.style.marginTop = 300 + "px";
 
 var div = 180 / 11;
 var radius = 400;
 var totalOffset_X = 600-40;
-var totalOffset_Y = 600;
+var totalOffset_Y = 450;
 for (var i = 0; i <= 11; ++i) {
     var name = "js-"+i.toString();
     const child = document.getElementById(name);
